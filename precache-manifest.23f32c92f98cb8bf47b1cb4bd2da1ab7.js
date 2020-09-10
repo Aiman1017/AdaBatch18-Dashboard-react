babcc8bb9c682ae7a82066e062365aeb965a20cf
Aiman Irfan <aimanirfan1017@gmail.com>
2020-09-10 21:01:25 +0800

    Updates

---
+++ precache-manifest.23f32c92f98cb8bf47b1cb4bd2da1ab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5af7878ac680ab6bd7d4ccf2bdb365c",
+    "url": "/AdaBatch18-Dashboard-react/index.html"
+  },
+  {
+    "revision": "ad636f68b9486f2a80f9",
+    "url": "/AdaBatch18-Dashboard-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6751758dbc39e4481818",
+    "url": "/AdaBatch18-Dashboard-react/static/js/2.02ac0663.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/AdaBatch18-Dashboard-react/static/js/2.02ac0663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad636f68b9486f2a80f9",
+    "url": "/AdaBatch18-Dashboard-react/static/js/main.d9cca8bc.chunk.js"
+  },
+  {
+    "revision": "5d9e045abea6694030ed",
+    "url": "/AdaBatch18-Dashboard-react/static/js/runtime-main.fbecbc44.js"
+  }
+]);
